cfa117b6b859751f29e7709ab29db832d803f1b5
jgignac2 <joseph.gignac@gmail.com>
2023-07-31 17:44:21 -0500

    Save to a file instead of printing

--- ctrlx_read_plcvars.py
+++ ctrlx_read_plcvars.py
@@ -1,5 +1,9 @@
-"""This program reads all datalayer nodes in the selected path"""
+"""
+This program reads all datalayer nodes in the selected path and saves them 
+as a json file
+"""
 import json
+import datetime
 import httpx
 
 URL = "https://192.168.0.5"
@@ -9,20 +13,32 @@ PASSWORD = "jpg"
 
 
 def main():
-    """Run the get_folder method on the selected path"""
+    """
+    Run the get_folder method on the selected path, save to user selected
+    filename when done.
+    """
     with httpx.Client(verify=False) as client:
         ctrlx = CtrlX(client, URL, USERNAME, PASSWORD)
         data = ctrlx.get_folder(PATH)
         json_text = json.dumps(data, indent=4, sort_keys=True)
 
-        print(json_text)
+        default_filename = (
+            "backup_" + datetime.datetime.now().strftime("%Y-%m-%dT%H-%M") + ".json"
+        )
+        filename = input(f"Enter a filename (Leave blank for {default_filename}):")
+        if filename == "":
+            filename = default_filename
+
+        with open(filename, "w", encoding="utf-8") as file:
+            file.write(json_text)
 
 
 class CtrlX:
     """ctrlX class"""
+
     def __init__(self, client, url, username, password):
         """
-        The initial method passes the httpx client, and connection info, then 
+        The initial method passes the httpx client, and connection info, then
         create the connection by getting the token.
         """
         self.client = client
