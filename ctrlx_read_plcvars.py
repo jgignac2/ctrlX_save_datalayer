af242a01a4f177b638956451c70b93037ac1db48
jgignac2 <joseph.gignac@gmail.com>
2023-08-02 21:57:35 -0500

    Put CtrlX class in seperate file

--- ctrlx_read_plcvars.py
+++ ctrlx_read_plcvars.py
@@ -5,6 +5,7 @@ as a json file
 import json
 import datetime
 import httpx
+from ctrlx import CtrlX
 
 URL = "https://192.168.1.1"
 PATH = "plc/app/Application/sym"
@@ -31,43 +32,6 @@ def main():
 
         with open(filename, "w", encoding="utf-8") as file:
             file.write(json_text)
-
-
-class CtrlX:
-    """ctrlX class"""
-
-    def __init__(self, client, url, username, password):
-        """
-        The initial method passes the httpx client, and connection info, then
-        create the connection by getting the token.
-        """
-        self.client = client
-        token_url = f"{url}/identity-manager/api/v2/auth/token?dryrun=false"
-        payload = {"name": username, "password": password}
-        token = self.client.post(token_url, json=payload)
-        self.token = {"Authorization": "Bearer " + token.json()["access_token"]}
-        self.url = url
-
-    def get_data(self, url):
-        """Do a GET using the token to read data"""
-        return self.client.get(f"{self.url}/{url}", headers=self.token)
-
-    def get_folder(self, url):
-        """
-        Read the folder contents, if it is a variable return the variable.
-        If it is a folder call the get_folder method to get its values.
-        This method is recursive.
-        """
-        folder = self.get_data(f"automation/api/v2/nodes/{url}")
-        if folder.status_code in (404, 501):
-            sub_items = self.get_data(f"automation/api/v2/nodes/{url}?type=browse")
-            item_values = {}
-            for sub_item in sub_items.json()["value"]:
-                item_value = self.get_folder(f"{url}/{sub_item}")
-                item_values[sub_item] = item_value
-            return item_values
-
-        return folder.json()
 
 
 if __name__ == "__main__":
